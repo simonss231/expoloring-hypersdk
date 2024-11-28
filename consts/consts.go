06985db99271fda3e8feeb16f4d7b04a039840a7
simonss231 <simonns121@gmail.com>
2024-11-28 15:16:51 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "meta"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "metachain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "MC"
 )
 
 var ID ids.ID
